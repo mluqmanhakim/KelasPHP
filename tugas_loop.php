4cbb0cd9c83b0d3c610257d34b979d30ff67ffd3
mluqmanhakim <mhd.luqmanhakim@gmail.com>
2020-10-20 08:40:59 +0700

    add tugas loop

---
+++ tugas_loop.php
@@ -0,0 +1,42 @@
+<?php
+
+# Buat 10 objek pelamar kerja
+
+class PelamarKerja {
+    function PelamarKerja($nama, $umur, $status) {
+        $this -> nama = $nama;
+        $this -> umur = $umur;
+        $this -> status = $status;
+    }
+}
+
+$pelamarKerja1 = new PelamarKerja("Acha", 20, "-");
+$pelamarKerja2 = new PelamarKerja("Beni", 15, "-");
+$pelamarKerja3 = new PelamarKerja("Citra", 16, "-");
+$pelamarKerja4 = new PelamarKerja("Dina", 45, "-");
+$pelamarKerja5 = new PelamarKerja("Eros", 29, "-");
+$pelamarKerja6 = new PelamarKerja("Fitrah", 27, "-");
+$pelamarKerja7 = new PelamarKerja("Gifari", 30, "-");
+$pelamarKerja8 = new PelamarKerja("Hanafi", 20, "-");
+$pelamarKerja9 = new PelamarKerja("Ihsan", 40, "-");
+$pelamarKerja10 = new PelamarKerja("Jana", 50, "-");
+
+# Masukkan objek ke dalam array
+
+$kumpulanPelamarKerja = [$pelamarKerja1, $pelamarKerja2, $pelamarKerja3, $pelamarKerja4, $pelamarKerja5, $pelamarKerja6, $pelamarKerja7, $pelamarKerja8, $pelamarKerja9, $pelamarKerja10];
+
+# iterasi pelamar kerja dengan foreach
+
+foreach ($kumpulanPelamarKerja as $pelamar) {
+    if ($pelamar -> umur > 17 && $pelamar -> umur <= 30) {
+        $pelamar -> status = "Lolos seleksi";
+    }
+    else {
+        $pelamar -> status = "Tidak Lolos seleksi";
+    }
+
+    echo $pelamar -> nama . " - " . $pelamar -> umur . " - " . $pelamar -> status . "<br>";
+
+}
+
+?>
